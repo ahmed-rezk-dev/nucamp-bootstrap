ba63fb73b2b15bd07f705a87aca23607ec47006c
Ahmed Rezk <work72019@gmail.com>
2020-02-22 09:51:24 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -25,4 +25,13 @@ $(function() {
 				.addClass("fa-pause");
 		}
 	});
+
+	// Reserve Modal
+	$("#reserveModalBtn").click(function() {
+		$(".reserveModal").modal("toggle");
+	});
+	// Login Modal
+	$("#loginModalBtn").click(function() {
+		$("#loginModal").modal("toggle");
+	});
 });
